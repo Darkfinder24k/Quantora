658d53c82b714b40b66870d8ae60eb6f6c2fcafd
Darkfinder24k <shivikush77@gmail.com>
2025-05-09 21:41:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -274,6 +274,8 @@ with st.container():
             except Exception as e:
                 st.error(f"An error occurred while processing your request: {e}")
         st.query_params.clear() # Reset query parameters, effectively clearing the input
-    st.markdown('</div>', unsafe_allow_html=True)# ✅ Footer
+    st.markdown('</div>', unsafe_allow_html=True)
+
+# ✅ Footer
 st.markdown("<hr style='border-top: 1px dashed #8c8b8b;'>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: #777;'>⚛️ Powered by Quantora AI</p>", unsafe_allow_html=True)
