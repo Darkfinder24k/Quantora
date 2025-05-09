246669b5c7ae9a0ef00f3e8cf88db765e9937278
Darkfinder24k <shivikush77@gmail.com>
2025-05-09 18:01:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ with st.container():
                 st.session_state.chat.append(("quantora", animated_response))
             except Exception as e:
                 st.error(f"An error occurred while processing your request: {e}")
-        st.experimental_set_query_params() # Reset query parameters, effectively clearing the input
+        st.query_params.clear() # Reset query parameters, effectively clearing the input
     st.markdown('</div>', unsafe_allow_html=True)# ✅ Footer
 st.markdown("<hr style='border-top: 1px dashed #8c8b8b;'>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: #777;'>⚛️ Powered by Quantora AI</p>", unsafe_allow_html=True)
