ce9637733bcae0e09776c1d79b42d190bf51c4ed
Darkfinder24k <shivikush77@gmail.com>
2025-05-11 19:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -387,7 +387,7 @@ st.markdown('<div class="send-box">', unsafe_allow_html=True)
 with st.form(key="chat_form", clear_on_submit=True):
     col1, col2, col3 = st.columns([4, 1, 1])
     user_input = col1.text_input("💬 Ask Quantora anything...", key="user_prompt_input", label_visibility="collapsed")
-    news_button = col2.form_submit_button("📰 News")
+    social_media_button = col2.form_submit_button("📱 Social")
     submitted = col3.form_submit_button("🚀 Send")
 st.markdown('</div>', unsafe_allow_html=True)
 
@@ -414,12 +414,12 @@ if use_mic:
                 except Exception as e:
                     st.error(f"An error occurred while processing your request: {e}")
 
-if news_button:
-    st.info("📰 Fetching the latest news...")
+if social_media_button:
+    st.info("Getting You Quantora Social Media ")
     # Replace this with your actual news fetching logic
     news_placeholder = st.empty()
     time.sleep(1)
-    st.markdown("[Click here to open Quatora News 📰](https://quantoranews.streamlit.app)", unsafe_allow_html=True)
+    st.markdown("[Click here to open Quatora Social Media📰](https://firebox-social.streamlit.app)", unsafe_allow_html=True)
 
 if submitted and user_input:
     st.session_state.chat.append(("user", user_input))
