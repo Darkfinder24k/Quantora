e81215a29ecfc59f0f5d0ec24652967a78cbee87
Darkfinder24k <shivikush77@gmail.com>
2025-06-22 23:50:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -850,6 +850,7 @@ def call_quantora_unified(prompt, context="", image=None):
             "provider-3/deepseek-v3",
             "provider-6/claude-3-7-sonnet-20250219-thinking",
             "provider-6/claude-sonnet-4-20250514-thinking"
+            "provider-5/gemini-2.5-flash-preview-05-20"
         ]
         
         for model in a4f_models:
