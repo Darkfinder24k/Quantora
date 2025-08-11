8ca6545bb3b7fba69292bf6a4e6a02e72d00c304
Kushagra Srivastava <shivikush77@gmail.com>
2025-08-11 17:10:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 from google.generativeai import types
+from google import genai
+from google.genai import types
 from datetime import datetime
 import time
 import os
